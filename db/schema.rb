9770dac9c27988bdb033b7189b7c77367616c889
caiguo <caiguo37@gmail.com>
2013-10-21 10:33:18 +0800

    修改开发环境的数据库为sqlite3

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20130905204142) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "delayed_jobs", force: true do |t|
     t.integer  "priority",   default: 0
     t.integer  "attempts",   default: 0
@@ -25,43 +22,43 @@ ActiveRecord::Schema.define(version: 20130905204142) do
     t.datetime "failed_at"
     t.string   "locked_by"
     t.string   "queue"
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
+  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority"
 
   create_table "feeds", force: true do |t|
     t.string   "name"
-    t.text     "url"
+    t.text     "url",          limit: 255
     t.datetime "last_fetched"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "status"
   end
 
-  add_index "feeds", ["url"], name: "index_feeds_on_url", unique: true, using: :btree
+  add_index "feeds", ["url"], name: "index_feeds_on_url", unique: true
 
   create_table "stories", force: true do |t|
-    t.text     "title"
-    t.text     "permalink"
+    t.text     "title",       limit: 255
+    t.text     "permalink",   limit: 255
     t.text     "body"
     t.integer  "feed_id"
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.datetime "published"
     t.boolean  "is_read"
-    t.boolean  "keep_unread", default: false
-    t.boolean  "is_starred",  default: false
-    t.text     "entry_id"
+    t.boolean  "keep_unread",             default: false
+    t.boolean  "is_starred",              default: false
+    t.text     "entry_id",    limit: 255
   end
 
-  add_index "stories", ["entry_id", "feed_id"], name: "index_stories_on_entry_id_and_feed_id", unique: true, using: :btree
+  add_index "stories", ["entry_id", "feed_id"], name: "index_stories_on_entry_id_and_feed_id", unique: true
 
   create_table "users", force: true do |t|
     t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.boolean  "setup_complete"
     t.string   "api_key"
   end
